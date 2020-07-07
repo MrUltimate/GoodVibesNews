23bf64e645a256aee7b67ca467c43f8172c89874
Shivam Sinha <sinha.shivam199@gmail.com>
2020-07-07 02:13:32 -0400

    Add Header WIP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.scss";
+import smiley from "./images/smiley.png";
 
 //Components
 import TopStories from "./components/TopStories";
@@ -12,15 +13,38 @@ import Business from "./components/Business";
 class App extends React.Component {
   render() {
     return (
-      <main className="App">
-        {/* <World /> */}
-        <TopStories />
-        <Science />
-        <Tech />
-        <Health />
-        <Business />
-        <Arts />
-      </main>
+      <>
+        <header>
+          <span className="date"></span>
+          <div className="logo">
+            <div className="logoInteractive">
+              G
+              <div className="smileyContainer">
+                <span className="smiley"></span>
+              </div>
+              <div className="smileyContainer">
+                <span className="smiley"></span>
+              </div>
+              d
+            </div>
+            <div>Vibe News.</div>
+          </div>
+          <div className="links">
+            <a href="#">About</a>
+            <a href="#">Donate</a>
+            <a href="#">Credits</a>
+          </div>
+        </header>
+        <main className="App">
+          {/* <World /> */}
+          <TopStories />
+          <Science />
+          <Tech />
+          <Health />
+          <Business />
+          <Arts />
+        </main>
+      </>
     );
   }
 }
--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,21 +1,30 @@
 import React from "react";
+import { removeWidows, defaultOpts, version } from "string-remove-widows";
+
+function makeMarkup(string) {
+  return {
+    __html: string,
+  };
+}
 
 function ArticleCard({ article, index }) {
   return (
     <a className="card" href={article.short_url} target="_blank" rel="noopener noreferrer">
-      <div
-        className="cardImage"
-        style={{
-          backgroundImage: `url('${article.multimedia[0].url}')`,
-        }}
-      ></div>
+      <div className="cardImageParent">
+        <div
+          className="cardImage"
+          style={{
+            backgroundImage: `url('${article.multimedia[0].url}')`,
+          }}
+        ></div>
+      </div>
       <div className="cardInfo">
         <div className="tags">
           <span className="category">{article.section}</span>
-          <span className="score">Confidence: {article.score}</span>
+          <span className="score">Score: {article.score}</span>
         </div>
-        <h1>{article.title}</h1>
-        <p>{article.abstract}</p>
+        <h1 dangerouslySetInnerHTML={makeMarkup(removeWidows(article.title).res)}></h1>
+        <p dangerouslySetInnerHTML={makeMarkup(removeWidows(article.abstract).res)}></p>
       </div>
     </a>
   );
